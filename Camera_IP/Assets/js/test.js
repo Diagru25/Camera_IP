a7fafec0c0acc43c24c3e7425957b358fc0fd5a1
Diagru25 <31183495+Diagru25@users.noreply.github.com>
2018-08-29 22:27:26 +0700

    toi 29/8

--- Camera_IP/Assets/js/test.js
+++ Camera_IP/Assets/js/test.js
@@ -1,18 +1,19 @@
 ﻿//Webcam
-//var video = document.querySelector("#videoElement");
+var video = document.querySelector("#videoElement");
 
-//if (navigator.mediaDevices.getUserMedia) {
-//    navigator.mediaDevices.getUserMedia({ video: true })
-//  .then(function (stream) {
-//      video.srcObject = stream;
-//  })
-//  .catch(function (err0r) {
-//      console.log("Something went wrong!");
-//  });
-//}
+if (navigator.mediaDevices.getUserMedia) {
+    navigator.mediaDevices.getUserMedia({ video: true })
+  .then(function (stream) {
+      video.srcObject = stream;
+  })
+  .catch(function (err0r) {
+      console.log("Something went wrong!");
+  });
+}
 
 // chọn số lượng
 $('#number_camera').click(function () {
+    var num_camera = 8;
     var num = $('#select_num_camera :selected').val();
     var i = 0;
     for(i; i < num; i++)
@@ -22,9 +23,98 @@ $('#number_camera').click(function () {
         $(id_box).removeClass("hidenbox");
     }
 
-    for (var j = num; j < 8; j++) {
+    for (var j = num; j < num_camera; j++) {
         var id_box = "#box" + j;
 
         $(id_box).addClass("hidenbox");
+    }
+})
+
+// cac button full man hinh dc click
+
+//button 0
+$('#btn-box0').click(function () {
+    $('#view_camera').empty();
+
+    var html_camera = "<img style='width: 250px;' class='img-responsive' src='http://169.254.252.253/nphMotionJpeg?Resolution=192x144&Quality=Standard' />";
+
+    $('#view_camera').append(html_camera);
+})
+
+//button 2
+$('#btn-box2').click(function () {
+    $('#view_camera').empty();
+
+    var html_camera = "<img style='width: 250px;' class='img-responsive' src='http://169.254.252.253/nphMotionJpeg?Resolution=192x144&Quality=Standard' />";
+
+    $('#view_camera').append(html_camera);
+})
+
+//button 3
+$('#btn-box3').click(function () {
+    $('#view_camera').empty();
+
+    var html_camera = "<img style='width: 250px;' class='img-responsive' src='http://169.254.252.253/nphMotionJpeg?Resolution=192x144&Quality=Standard' />";
+
+    $('#view_camera').append(html_camera);
+})
+
+//button 4
+$('#btn-box4').click(function () {
+    $('#view_camera').empty();
+
+    var html_camera = "<img style='width: 250px;' class='img-responsive' src='http://169.254.252.253/nphMotionJpeg?Resolution=192x144&Quality=Standard' />";
+
+    $('#view_camera').append(html_camera);
+})
+
+//button 5
+$('#btn-box5').click(function () {
+    $('#view_camera').empty();
+
+    var html_camera = "<img style='width: 250px;' class='img-responsive' src='http://169.254.252.253/nphMotionJpeg?Resolution=192x144&Quality=Standard' />";
+
+    $('#view_camera').append(html_camera);
+})
+
+//button 6
+$('#btn-box6').click(function () {
+    $('#view_camera').empty();
+
+    var html_camera = "<img style='width: 250px;' class='img-responsive' src='http://169.254.252.253/nphMotionJpeg?Resolution=192x144&Quality=Standard' />";
+
+    $('#view_camera').append(html_camera);
+})
+
+//button 7
+$('#btn-box7').click(function () {
+    $('#view_camera').empty();
+
+    var html_camera = "<img style='width: 250px;' class='img-responsive' src='http://169.254.252.253/nphMotionJpeg?Resolution=192x144&Quality=Standard' />";
+
+    $('#view_camera').append(html_camera);
+})
+
+
+//button 1
+$('#btn-box1').click(function () {
+
+    // clear the div
+    $('#view_camera').empty();
+
+    var html_camera = "<video autoplay='true' id='videoElement_modal' style='max-width:100%;'></video>";
+
+    $('#view_camera').append(html_camera);
+
+    var video = document.querySelector("#videoElement_modal");
+
+    if (navigator.mediaDevices.getUserMedia) {
+        navigator.mediaDevices.getUserMedia({ video: true })
+      .then(function (stream) {
+          video.srcObject = stream;
+      })
+      .catch(function (err0r) {
+          console.log("Something went wrong!");
+      });
     }
 })
