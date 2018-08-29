d74fb9468bc4aad2b1159297043a3914523bba70
Diagru25 <31183495+Diagru25@users.noreply.github.com>
2018-08-29 07:39:04 +0700

    new

--- Camera_IP/Assets/js/test.js
+++ Camera_IP/Assets/js/test.js
@@ -1,17 +1,17 @@
 ﻿//Webcam
-var video = document.querySelector("#videoElement");
+//var video = document.querySelector("#videoElement");
 
-if (navigator.mediaDevices.getUserMedia) {
-    navigator.mediaDevices.getUserMedia({ video: true })
-  .then(function (stream) {
-      video.srcObject = stream;
-  })
-  .catch(function (err0r) {
-      console.log("Something went wrong!");
-  });
-}
+//if (navigator.mediaDevices.getUserMedia) {
+//    navigator.mediaDevices.getUserMedia({ video: true })
+//  .then(function (stream) {
+//      video.srcObject = stream;
+//  })
+//  .catch(function (err0r) {
+//      console.log("Something went wrong!");
+//  });
+//}
 
-// the video
+// chọn số lượng
 $('#number_camera').click(function () {
     var num = $('#select_num_camera :selected').val();
     var i = 0;
@@ -19,12 +19,12 @@ $('#number_camera').click(function () {
     {
         var id_box = "#box" + i;
 
-        $(id_box).removeClass("invisible");
+        $(id_box).removeClass("hidenbox");
     }
 
     for (var j = num; j < 8; j++) {
         var id_box = "#box" + j;
 
-        $(id_box).addClass("invisible");
+        $(id_box).addClass("hidenbox");
     }
 })
